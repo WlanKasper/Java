189fbc316076b30040b466318dec32babd9135ae
Arthur Smirnov <arthursmirnov@MBP-Arthur.wind3.hub>
2022-01-22 21:28:31 +0100

    IN-Carte | v0.0

---
+++ WorkSpace_IN/Carte/src/wlankasper/com/carte/Main.java
@@ -0,0 +1,9 @@
+package wlankasper.com.carte;
+
+import wlankasper.com.carte.layout.Console;
+
+public class Main {
+    public static void main(String[] args) {
+        new Console();
+    }
+}
---
+++ WorkSpace_IN/Carte/src/wlankasper/com/carte/decks/Deck.java
@@ -0,0 +1,20 @@
+package wlankasper.com.carte.decks;
+
+public class Deck {
+
+    TypeDeck deck;
+
+    public Deck(TypeDeck deck) {
+        this.deck = deck;
+    }
+
+    public fillDeck () {
+
+    }
+
+    @Override
+    public String toString (){
+        String s = "";
+        return s;
+    }
+}
